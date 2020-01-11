a0bfe3f9c70a103ff175a8ca31ce7c29c17fead1
Sourabh Subhod <sourabhsubhod@gmail.com>
2020-01-11 18:05:35 +0530

    some more changes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,7 +7,86 @@ import Layout from '../components/layout'
 
 import heroStyles from '../components/hero.module.css'
 
+const comments = props => {
+  const order = props.order === true ? "Oldest First" : "Most Recent"
+  return (
+    <>
+      <div>
+        {props.comments && <span>{props.comments.length} Comments</span>}
+        <button onClick={props.sortComments}>{order}</button>
+      </div>
+      {props.comments &&
+        props.comments.map(comment => (
+          <div key={comment.id}>
+            <div >
+              <div >
+                <span >
+                  <a href={comment.handle}>{comment.name}</a>
+                </span>
+                {/* <span className={styles.commentDetailsDate}>
+                  {formatDate(comment.timestamp)}
+                </span> */}
+              </div>
+              <p>{comment.message}</p>
+            </div>
+            <div >
+              {comment.replies.map((comment, index) => (
+                <div key={index}>
+                  <div>
+                    <span>
+                      <a href={comment.handle}>{comment.name}</a>
+                    </span>
+                    {/* <span className={styles.commentDetailsDate}>
+                      {formatDate(comment.timestamp)}
+                    </span> */}
+                  </div>
+                  <p>{comment.message}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+    </>
+  )
+}
+
 class BlogPostTemplate extends React.Component {
+  componentDidMount() {
+    const ID = this.props.data.contentfulBlogPost.contentful_id
+    axios.get("/.netlify/functions/test", {
+      params: {
+        ID: ID
+      }
+    }).then(response => {
+      this.setState({
+        comments: response.data.comments
+      })
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+  sortComments = () => {
+    const comments = this.state.comments
+    if (!this.state.sorting) {
+      const sorted = comments.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1))
+      this.setState({
+        comments: sorted,
+        sorting: true
+      })
+    } else {
+      const sorted = comments.sort((a, b) => (a.timestamp > b.timestamp ? -1 : 1))
+      this.setState({
+        comments: sorted,
+        sorting: false
+      })
+    }
+  }
+  handleChange = evt => {
+    evt.persist()
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
@@ -33,6 +112,16 @@ class BlogPostTemplate extends React.Component {
                 __html: post.body.childMarkdownRemark.html,
               }}
             />
+            <div>
+              {
+                this.props.data.contentfulBlogPost.enableComments && (
+                  <Comments
+                    comments={this.state.comments}
+                    sortComments={this.sortComments}
+                    order={this.state.sorting}
+                  />)
+              }
+            </div>
           </div>
         </div>
       </Layout>
